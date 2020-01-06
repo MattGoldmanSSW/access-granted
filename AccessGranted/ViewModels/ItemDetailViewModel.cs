d7513d9a4474daef8d877c98bac8f0be3e6dcb2f
Matt Goldman <mattgoldman@ssw.com.au>
2020-01-06 16:00:09 +1100

    Initial build

---
+++ AccessGranted/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using AccessGranted.Models;
+
+namespace AccessGranted.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
