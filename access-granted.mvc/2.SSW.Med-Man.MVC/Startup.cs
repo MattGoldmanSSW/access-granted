7f1e615836b6af70c189848f8a4ba5e2d23cecf2
Matt Goldman <MattGoldman@ssw.com.au>
2020-01-30 09:01:37 +1100

    Role logins now working, updated home screen and menus

--- access-granted.mvc/2.SSW.Med-Man.MVC/Startup.cs
+++ access-granted.mvc/2.SSW.Med-Man.MVC/Startup.cs
@@ -32,8 +32,11 @@ namespace SSW.Med_Man.MVC
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
+                .AddRoles<IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
+
             services.AddControllersWithViews();
             services.AddRazorPages();
             services.AddMvc(config =>
