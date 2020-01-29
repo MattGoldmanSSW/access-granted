481f391bbcafec653e886c9a0f5da58a3110226f
Matt Goldman <mattgoldman1981@gmail.com>
2020-01-30 07:07:53 +1100

    Added role and user seed method

--- access-granted.mvc/2.SSW.Med-Man.MVC/Data/ApplicationDbContext.cs
+++ access-granted.mvc/2.SSW.Med-Man.MVC/Data/ApplicationDbContext.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using SSW.Med_Man.MVC.Models;
@@ -18,6 +20,24 @@ namespace SSW.Med_Man.MVC.Data
         public DbSet<Medication> Medications { get; set; }
         public DbSet<Prescription> Prescriptions { get; set; }
         public DbSet<Administrations> Administrations { get; set; }
-        
+
+        public async Task SeedRolesAsync(RoleManager<IdentityRole> roleManager, UserManager<IdentityUser> userManager)
+        {
+            if (!await roleManager.RoleExistsAsync("Admin"))
+            {
+                var admin = new IdentityRole("Admin");
+                await roleManager.CreateAsync(admin);
+
+                IdentityUser user = new IdentityUser
+                {
+                    UserName = "admin@admin.com",
+                    Email = "admin@admin.com",
+                };
+
+                await userManager.CreateAsync(user, "DefaultAdminPassword1!");
+                await userManager.AddToRoleAsync(user, "Admin");
+            }
+        }
+
     }
 }
