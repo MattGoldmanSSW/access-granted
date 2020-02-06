bcc874fd1fe5233dea739095996c28b18545248c
Matt Goldman <mattgoldman1981@gmail.com>
2020-02-06 21:38:42 +1100

    Patient and med filters added

--- access-granted.mvc/3.SSW.Med-Man.SPA/Client/ssw-med-man/src/app/add-prescription/add-prescription.component.ts
+++ access-granted.mvc/3.SSW.Med-Man.SPA/Client/ssw-med-man/src/app/add-prescription/add-prescription.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { PrescriptionDTO, PrescriptionsClient, PatientsClient, MedicationsClient, PatientDTO } from '../../helpers/api-client';
+import { PrescriptionDTO, PrescriptionsClient, PatientsClient, MedicationsClient, PatientDTO, MedicationDTO } from '../../helpers/api-client';
 import { Router } from '@angular/router';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
@@ -20,6 +20,10 @@ export class AddPrescriptionComponent implements OnInit {
   patients: PatientDTO[] = [];
   filteredPatients: Observable<PatientDTO[]>;
 
+  medications: MedicationDTO[] = [];
+  filteredMeds: Observable<MedicationDTO[]>;
+
+  medicationControl = new FormControl;
   patientControl = new FormControl;
 
   ngOnInit() {
@@ -33,11 +37,27 @@ export class AddPrescriptionComponent implements OnInit {
         startWith(''),
         map(value => typeof value === 'string' ? value : value.fullName),
         map(name => name ? this._filterPatients(name) : this.patients.slice())
-      );
+    );
+
+    this.medicationsClient.getMedications()
+    .subscribe(result => {
+      this.medications = result;
+    });
+
+    this.filteredMeds = this.medicationControl.valueChanges
+    .pipe(
+      startWith(''),
+      map(value => typeof value === 'string' ? value : value.name),
+      map(name => name ? this._filterMeds(name) : this.medications.slice())
+    );
   }
 
   displayPatientFn(patient?: PatientDTO): string | undefined {
     return patient ? patient.fullName : undefined;
+  }
+
+  displayMedicationFn(medication?: MedicationDTO): string | undefined {
+    return medication ? medication.name : undefined;
   }
 
   addPatient({value} : {value : PrescriptionDTO }){
@@ -61,6 +81,12 @@ export class AddPrescriptionComponent implements OnInit {
     const filterValue = value.toLowerCase();
 
     return this.patients.filter(patient => patient.fullName.toLowerCase().includes(filterValue));
+  }
+
+  private _filterMeds(value: string): MedicationDTO[] {
+    const filterValue = value.toLowerCase();
+
+    return this.medications.filter(med => med.name.toLowerCase().includes(filterValue));
   }
 
 }
