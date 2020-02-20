e0d6c0ad2127a384fd43a0fc81ae0136d355380c
Matt Goldman <MattGoldman@ssw.com.au>
2020-02-21 06:03:59 +1100

    Configured clients to use ssl

--- 4.SSW.Med-Man.B2C/Client/ssw-med-man/src/app/app.module.ts
+++ 4.SSW.Med-Man.B2C/Client/ssw-med-man/src/app/app.module.ts
@@ -100,7 +100,7 @@ export function JwtTokenGetter() {
     }),
     MsalModule.forRoot({
       clientID: "d31fe268-a410-4a9f-9ce0-a91078f75396",
-      authority: "https://goldiessw.b2clogin.com/tfp/goldiessw.onmicrosoft.com/B2C_1_signupandsignin",
+      authority: "https://goldiessw.b2clogin.com/tfp/goldiessw.onmicrosoft.com/B2C_1A_signup_signin",
       redirectUri: "https://localhost:4200",
       validateAuthority : false,
       cacheLocation : "localStorage",
